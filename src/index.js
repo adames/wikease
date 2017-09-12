4749637f768568a8d1d549439b534a55befe0654
adames <adameshh@gmail.com>
2017-09-12 15:57:43 -0400

    made single css file

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
-import 'react-responsive-carousel/lib/styles/carousel.css';
-import 'react-responsive-carousel/lib/styles/examples/presentation/presentation.css';
+// import 'react-responsive-carousel/lib/styles/carousel.css';
+// import 'react-responsive-carousel/lib/styles/examples/presentation/presentation.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
